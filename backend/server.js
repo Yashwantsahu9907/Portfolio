ca6a113510c7456c52f4c8ca65a80507a16ebd03
Yashwant sahu <sahuyashwant000@gmail.com>
2026-05-12 07:45:20 +0530

    File structure

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import connectDB from './config/db.js';
+import { fileURlToPath } from 'url';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running. ');
+
+});
+
+connectDB();
+
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
