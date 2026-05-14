e1547eb7d14340ae0ea3bcdd7494884383691ce4
Yashwant sahu <sahuyashwant000@gmail.com>
2026-05-14 18:44:07 +0530

    token generator

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,1 +1,15 @@
+import jwt from 'jsonwebtoken';   // import jwt package for generate token
+
+// function for recieve admin id and generate token
+const generateToken = (id) => {
+
+    //jwt.sign create token 
+    return jwt.sign({ id }, process.env.JWT_SECRET || 'supersecretkey_change_me_in_prod', //supersec....:- .env me secret key nhi mila to default use karega
+        {
+            expiresIn: '30d',  // token expire in 30 days
+        }
+    );
+};
+
+export default generateToken;
 
